9be8ddb12d8310ab45e70ce262111e653cf14b7e
tasneem04 <eng.wr.tasneem@gmail.com>
2024-05-26 17:40:04 +0300

    add the pages and wedgits

---
+++ lib/presentaion/pages/main_page.dart
@@ -0,0 +1,43 @@
+import 'package:calculatorclean/presentaion/widgets/main_text.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(children: [
+          SizedBox(
+            height: 150,
+          ),
+          Align(alignment: Alignment.topRight, child: MainText()),
+          Container(
+            child: GridView.builder(
+                itemCount: 10,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4),
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 30,
+                    width: 30,
+                    padding: EdgeInsets.all(5),
+                    color: Colors.blue,
+                  );
+                }),
+          )
+        ]),
+      ),
+    );
+  }
+}
---
+++ lib/presentaion/widgets/main_text.dart
@@ -0,0 +1,17 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class MainText extends StatelessWidget {
+  int numText;
+
+  MainText({this.numText = 0, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      numText.toString(),
+      style: TextStyle(fontSize: 90),
+    );
+  }
+}
